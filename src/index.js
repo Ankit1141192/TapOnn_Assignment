117987e2a8e72775d6af9d532310557202578e5f
Ankit Kumar <ankit2914978@gmail.com>
2025-12-18 22:07:20 +0530

    delete

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const env = require("./config/env");
-
-const PORT = env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  console.log(`Environment: ${process.env.NODE_ENV}`);
-});
